a2a994f23b1b64722f31c797341b767a20f09eaa
binhsna <99773978+binhsna@users.noreply.github.com>
2024-06-06 13:42:54 +0700

    Desgin Order Page part 01
    Thiết kế trang đặt hàng Order

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {OrderComponent} from './order/order.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    //HomeComponent,
+    OrderComponent,
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
