c59cc4a1491398de4fdabe3b2a4a21959c8acc58
binhsna <99773978+binhsna@users.noreply.github.com>
2024-06-07 12:06:57 +0700

    Design table tokens, social accounts, products, categories
    Thiết kế bảng tokens, social accounts, products, categories

--- shopapp-backend/database.sql
+++ shopapp-backend/database.sql
@@ -14,4 +14,40 @@ CREATE TABLE users(
     facebook_account_id INT DEFAULT 0,
     google_account_id INT DEFAULT 0
 );
-
+CREATE TABLE tokens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    expiration_date DATETIME,
+    revoked TINYINT(1) NOT NULL,
+    expired TINYINT(1) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+-- Hỗ trợ đăng nhập từ Facebook và Google
+CREATE TABLE social_accounts(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `provider` VARCHAR(20) NOT NULL COMMENT 'Tên nhà social network',
+    `provider_id` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(150) NOT NULL COMMENT 'Email tài khoản',
+    `name` VARCHAR(100) NOT NULL COMMENT 'Tên người dùng',
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+-- Bảng danh mục sản phẩm (Category)
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT '' COMMENT 'Tên danh mục'
+);
+-- Bảng chứa sản phẩm (Product)
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(350) COMMENT 'Tên sản phẩm',
+    price FLOAT NOT NULL CHECK(price >= 0),
+    thumbnail VARCHAR(300) DEFAULT '',
+    description LONGTEXT DEFAULT '',
+    created_at DATETIME,
+    updated_at DATETIME,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
