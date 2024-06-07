487b626130efa29879e799fa3297b4de2180fd69
binhsna <99773978+binhsna@users.noreply.github.com>
2024-06-07 21:46:48 +0700

    DTO & Input Validation
    Cài Postman, viết các request GET, POST, PUT, DELETE
    Data Transfer Object và Input Validation

---
+++ shopapp-backend/shopapp/src/main/java/com/binhnc/shopapp/controller/CategoryController.java
@@ -0,0 +1,52 @@
+package com.binhnc.shopapp.controller;
+
+import com.binhnc.shopapp.dto.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categories")
+//@Validated
+public class CategoryController {
+    // Hiển thị tất cả các categories
+    @GetMapping("") // http://localhost:8080/api/v1/categories?page=1&limit=10
+    public ResponseEntity<String> getAllCategories(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok(String.format("page = %d, limit = %d", page, limit));
+    }
+
+    // Thêm mới category
+    @PostMapping("")
+    // Nếu tham số truyền vào là 1 object thì sao? => Data Transfer Object = Request Object
+    public ResponseEntity<?> insertCategory(
+            @Valid @RequestBody CategoryDTO categoryDTO,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errorMessages = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("add new category " + categoryDTO);
+    }
+
+    // Update category
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("update category with id = " + id);
+    }
+
+    // Delete category
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("delete category with id = " + id);
+    }
+}
