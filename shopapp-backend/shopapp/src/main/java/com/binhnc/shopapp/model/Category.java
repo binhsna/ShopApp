1579a0abc5e77faf54e7aa4ac18615efe611efda
binhsna <99773978+binhsna@users.noreply.github.com>
2024-06-08 16:39:37 +0700

    Design model Category and Product
    Viết các model Category và Product

---
+++ shopapp-backend/shopapp/src/main/java/com/binhnc/shopapp/model/Category.java
@@ -0,0 +1,21 @@
+package com.binhnc.shopapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
