4613c06d115d40fb69e89ea0b3f4e237eb70ff4d
binhsna <99773978+binhsna@users.noreply.github.com>
2024-06-14 18:15:29 +0700

    2 way Data binding
    2 way Data binding trong angular

--- shopapp-angular/src/app/register/register.component.ts
+++ shopapp-angular/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  // Khai báo các biến tương ứng với các trường dữ liệu trong java
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
 
+  constructor() {
+    this.phone = "";
+    this.password = "";
+    this.retypePassword = "";
+    this.fullName = "";
+    this.address = "";
+    this.isAccepted = false;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
+  }
+
+  register() {
+    const message = `phone: ${this.phone}\n` +
+      `password: ${this.password}\n` +
+      `retypePassword: ${this.retypePassword}\n` +
+      `fullName: ${this.fullName}\n` +
+      `address: ${this.address}\n` +
+      `isAccepted: ${this.isAccepted}\n` +
+      `dateOfBirth: ${this.dateOfBirth}\n`
+    ;
+    alert(message);
+  }
 }
+
